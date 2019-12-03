e5dde0cd8b7ab43b45ec4ef50663001fda2c588f
Alex Lalejini <amlalejini@gmail.com>
2019-12-03 11:10:07 -0500

    Add RandomBitSet and RandomBitSets functions to random_utils plus code for testing.

--- source/tools/random_utils.h
+++ source/tools/random_utils.h
@@ -11,9 +11,12 @@
 #ifndef EMP_RANDOM_UTILS_H
 #define EMP_RANDOM_UTILS_H
 
+#include <unordered_set>
 #include "../base/vector.h"
 #include "BitVector.h"
 #include "Random.h"
+#include "math.h"
+#include "set_utils.h"
 
 namespace emp {
 
@@ -109,6 +112,59 @@ namespace emp {
   template <typename T>
   inline void RandomizeVector(emp::vector<T> & vals, Random & random, T min, T max) {
     for (T & v : vals) v = (T) random.GetDouble((double) min, (double) max);
+  }
+
+  /// Generate one random BitSet<W>.
+  /// Given a vector of other BitSets (unique_from), this function will guarantee
+  /// the generated BitSet is unique with respect to those BitSets.
+  /// @param rnd - Random number generator to use.
+  /// @param unique_from - Other BitSets that the generated BitSet should be unique from.
+  template<size_t W>
+  BitSet<W> RandomBitSet(emp::Random & rnd, const emp::vector<BitSet<W>> & unique_from=emp::vector<BitSet<W>>()) {
+    std::unordered_set<BitSet<W>> unique_from_set(unique_from.begin(), unique_from.end());
+    emp_assert(unique_from_set.size() < emp::Pow2(W), "BitSet<W> is not large enough to be able to guarantee requested number of unique tags");
+    BitSet<W> new_bitset(rnd, 0.5);
+    while (unique_from_set.size()) {
+      if (emp::Has(unique_from_set, new_bitset)) {
+        new_bitset.Randomize(rnd);
+        continue;
+      }
+      break;
+    }
+    return new_bitset;
+  }
+
+  /// Generate 'count' number of random BitSet<W>.
+  /// Given a vector of other bitsets (unique_from), this function will guarantee the bitsets generated
+  /// and returned are unique with respect to unique_from.
+  /// @param rnd - Random number generator to use when generating a random bitset.
+  /// @param count - How many bitsets should be generated?
+  /// @param guarantee_unique - Should generated bitsets be guaranteed to be unique from each other?
+  /// @param unique_from - Other bitsets that the bitsets being generated should be unique with respect to. Only used if 'guarantee_unique' is true.
+  template<size_t W>
+  emp::vector<BitSet<W>> RandomBitSets(emp::Random & rnd, size_t count, bool guarantee_unique=false,
+                                       const emp::vector<BitSet<W>> & unique_from=emp::vector<BitSet<W>>())
+  {
+    // If we don't have to make any promises, run quickly!
+    emp::vector<BitSet<W>> new_bitsets(count);
+    if (!guarantee_unique && unique_from.size() == 0) {
+      for (auto & bs : new_bitsets) { bs.Randomize(rnd); }
+    } else {
+      std::unordered_set<BitSet<W>> unique_from_set(unique_from.begin(), unique_from.end());
+      emp_assert(unique_from_set.size()+count <= emp::Pow2(W), "Not possible to generate requested number of BitSets");
+      for (auto & bs : new_bitsets) {
+        bs.Randomize(rnd);
+        while (unique_from_set.size()) {
+          if (emp::Has(unique_from_set, bs)) {
+            bs.Randomize(rnd);
+            continue;
+          }
+          break;
+        }
+        unique_from_set.emplace(bs);
+      }
+    }
+    return new_bitsets;
   }
 }
 
